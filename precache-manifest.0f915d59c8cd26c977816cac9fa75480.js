559cd1f1eed9b6f0ac641e920e07e2eb2644c78a
Ian Gonzalez <iangagustin@gmail.com>
2019-07-26 19:45:22 -0300

    Updates

---
+++ precache-manifest.0f915d59c8cd26c977816cac9fa75480.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69550f2fc549e31d52af8a197dd830cc",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "f188065f6d7ef703a46c",
+    "url": "/pokedex/static/css/2.2b82a394.chunk.css"
+  },
+  {
+    "revision": "cdaf849de0ceccd28391",
+    "url": "/pokedex/static/css/main.9fae951a.chunk.css"
+  },
+  {
+    "revision": "f188065f6d7ef703a46c",
+    "url": "/pokedex/static/js/2.6a5d434b.chunk.js"
+  },
+  {
+    "revision": "cdaf849de0ceccd28391",
+    "url": "/pokedex/static/js/main.aba7f698.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  }
+]);
