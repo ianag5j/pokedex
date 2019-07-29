794d59dd75cfe466a663ac320e72cd31cd78f6aa
Ian Gonzalez <iangagustin@gmail.com>
2019-07-29 13:02:59 -0300

    🚧 Buscar Pokemon

--- src/components/layaout/NavBar.js
+++ src/components/layaout/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Axios from "axios";
 import {
   Navbar,
   Nav,
@@ -6,29 +7,77 @@ import {
   InputGroup,
   InputGroupAddon,
   InputGroupText,
-  FormInput
+  FormInput,
+  ListGroup,
+  ListGroupItem
 } from "shards-react";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      UrlAllsPokemons: "https://pokeapi.co/api/v2/pokemon/?limit=10000",
+      listaAllsPokemons: [],
+      resultadoPokemons: []
+    };
+    this.buscarPokemon = this.buscarPokemon.bind(this);
+  }
+
+  async componentDidMount() {
+    let listaPokemons = await Axios.get(this.state.UrlAllsPokemons);
+    this.setState({
+      listaAllsPokemons: listaPokemons.data.results
+    });
+  }
+
+  buscarPokemon(event) {
+    let busqueda = event.target.value;
+    if (busqueda !== "") {
+      let resultados = this.state.listaAllsPokemons.filter(pokemon =>
+        pokemon.name.includes(busqueda)
+      );
+      resultados = resultados.slice(0, 5);
+      this.setState({
+        resultadoPokemons: resultados
+      });
+      console.log(resultados);
+    } else {
+      this.setState({
+        resultadoPokemons: []
+      });
+    }
+  }
+
   render() {
     return (
       <div>
-        <Navbar type="dark" theme="danger" expand="md" sticky="sticky">
+        <Navbar type="dark" theme="danger" expand="md">
           <NavbarBrand href="/">
             <img src="Pokédex_logo.png" height="30" alt="Pokedex" />
           </NavbarBrand>
-          {/* <Nav navbar className="ml-auto">
-                        <InputGroup size="sm" seamless>
-                        <InputGroupAddon type="prepend">
-                            <InputGroupText>
-                            <FontAwesomeIcon icon={faSearch} />
-                            </InputGroupText>
-                        </InputGroupAddon>
-                        <FormInput className="border-0" placeholder="Buscar Pokemon" />
-                        </InputGroup>
-                    </Nav> */}
+          <Nav navbar className="ml-auto">
+            <InputGroup size="sm" seamless>
+              <InputGroupAddon type="prepend">
+                <InputGroupText>
+                  <FontAwesomeIcon icon={faSearch} />
+                </InputGroupText>
+              </InputGroupAddon>
+              <FormInput
+                id="buscador"
+                className="border-0"
+                placeholder="Buscar Pokemon"
+                onChange={this.buscarPokemon}
+              />
+              <ListGroup small={true} id="resultados-busqueda">
+                {this.state.resultadoPokemons.map(pokemon => {
+                  return <ListGroupItem>{pokemon.name}</ListGroupItem>;
+                })}
+              </ListGroup>
+              <div />
+            </InputGroup>
+          </Nav>
         </Navbar>
       </div>
     );
